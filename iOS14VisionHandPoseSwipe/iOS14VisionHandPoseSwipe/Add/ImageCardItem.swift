38e6f0e9ad040ac9bfc0b9bb79cc83df8df17b4a
risa <cherry881113@gmail.com>
2024-07-07 16:09:34 +0800

    update

---
+++ iOS14VisionHandPoseSwipe/iOS14VisionHandPoseSwipe/Add/ImageCardItem.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCardItem.swift
+//  iOS14VisionHandPoseSwipe
+//
+//  Created by 彭惠暄 on 2024/5/11.
+//
+
+import Foundation
---
+++ iOS14VisionHandPoseSwipe/iOS14VisionHandPoseSwipe/Add/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  iOS14VisionHandPoseSwipe
+//
+//  Created by 彭惠暄 on 2024/5/11.
+//
+
+import Foundation
